65aabeb30837e99be8e185528c1e9588ff0a9009
Fran Bosquet <fran@redradix.com>
2017-12-01 12:10:07 +0100

    Add todo: Pending test methods

--- tests.js
+++ tests.js
@@ -1,5 +1,8 @@
 var Test = require('assert')
+
+//TODO define every assert method fron codewars CW using node assert methods
 Test.assertEquals = Test.equal
+//...X methods left
 
 //load yout function
 var myFunc = require('./index')
